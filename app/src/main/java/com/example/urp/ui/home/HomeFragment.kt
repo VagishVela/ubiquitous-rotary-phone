aa68b131fdbdfe34c832d5aac271988ab5da1c3a
Classicalz2 <stevenalbert72@gmail.com>
2021-03-27 18:39:48 -0400

    Added Stretching

--- app/src/main/java/com/example/urp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/urp/ui/home/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.urp.MainActivity
@@ -56,9 +58,9 @@ class HomeFragment : Fragment() {
             when (MainActivity.activity++) {
                 0 -> {
                     imageView.setImageResource(R.drawable.pushup)
-                    point1Button.text = "Do 5 Pushup"
-                    point5Button.text = "Do 25 Pushup"
-                    point10Button.text = "Do 50 Pushup"
+                    point1Button.text = "Do 5 Pushups"
+                    point5Button.text = "Do 25 Pushups"
+                    point10Button.text = "Do 50 Pushups"
                     activityName.text = "Do some pushups!"
                 }
                 1 -> {
@@ -81,6 +83,13 @@ class HomeFragment : Fragment() {
                     point5Button.text = "Do 25 Situps"
                     point10Button.text = "Do 50 Situps"
                     activityName.text = "Do some situps!"
+                }
+                4 -> {
+                    imageView.setImageResource(R.drawable.stretch)
+                    point1Button.text = "Stretch 30 secs"
+                    point5Button.text = "Stretch 1 min"
+                    point10Button.text = "Stretch 2 mins"
+                    activityName.text = "Don't forget to stretch!"
                     MainActivity.activity = 0
                 }
             }
@@ -96,11 +105,11 @@ class HomeFragment : Fragment() {
             when (MainActivity.activity--) {
                 0 -> {
                     imageView.setImageResource(R.drawable.pushup)
-                    point1Button.text = "Do 5 Pushup"
-                    point5Button.text = "Do 25 Pushup"
-                    point10Button.text = "Do 50 Pushup"
+                    point1Button.text = "Do 5 Pushups"
+                    point5Button.text = "Do 25 Pushups"
+                    point10Button.text = "Do 50 Pushups"
                     activityName.text = "Do some pushups!"
-                    MainActivity.activity = 3 // This will change to max num of activities.
+                    MainActivity.activity = 4 // This will change to max num of activities.
                 }
                 1 -> {
                     imageView.setImageResource(R.drawable.bottledwater)
@@ -122,6 +131,13 @@ class HomeFragment : Fragment() {
                     point5Button.text = "Do 25 Situps"
                     point10Button.text = "Do 50 Situps"
                     activityName.text = "Do some situps!"
+                }
+                4 -> {
+                    imageView.setImageResource(R.drawable.stretch)
+                    point1Button.text = "Stretch 30 secs"
+                    point5Button.text = "Stretch 1 min"
+                    point10Button.text = "Stretch 2 mins"
+                    activityName.text = "Don't forget to stretch!"
                 }
             }
         }
