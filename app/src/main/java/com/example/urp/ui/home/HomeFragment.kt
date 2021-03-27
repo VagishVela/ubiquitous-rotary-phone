8f50344d1ba6372fe50131bbcf152609d3e089fd
BuildTools <unconfigured@null.spigotmc.org>
2021-03-27 12:40:03 -0400

    Added walk5m and walk10m buttons.

--- app/src/main/java/com/example/urp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/urp/ui/home/HomeFragment.kt
@@ -13,7 +13,6 @@ import com.example.urp.MainActivity
 import com.example.urp.R
 
 class HomeFragment : Fragment() {
-
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
@@ -21,22 +20,30 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
 
         val scoreText = root.findViewById<TextView>(R.id.score)
         val walk1m = root.findViewById<Button>(R.id.walk1m)
         walk1m.setOnClickListener { scoreText.text = "${++MainActivity.score}" }
+
+        val walk5m = root.findViewById<Button>(R.id.walk5m)
+        walk5m.setOnClickListener {
+            MainActivity.score += 5
+            scoreText.text = "${MainActivity.score}"
+        }
+
+        val walk10m = root.findViewById<Button>(R.id.walk10m)
+        walk10m.setOnClickListener {
+            MainActivity.score += 10
+            scoreText.text = "${MainActivity.score}"
+        }
+
         scoreText.text = "${MainActivity.score}"
         return root
     }
-
-
-
 }
