a70f0050bd95ce212b649a3203785c2dbe0d7214
BuildTools <unconfigured@null.spigotmc.org>
2021-03-26 21:33:20 -0400

    Fixed button listener.

--- app/src/main/java/com/example/urp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/urp/ui/home/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,17 +17,28 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val score = root.findViewById<TextView>(R.id.score)
+        val walk1m = root.findViewById<Button>(R.id.walk1m)
+        walk1m.setOnClickListener { testFun(root) }
+
         return root
+    }
+
+    fun testFun(view: View) {
+//        Toast.makeText(applicationContext, "Simple Button 1", Toast.LENGTH_LONG).show()
+        val walk1m = view.findViewById<Button>(R.id.walk1m)
+        walk1m.text = "${walk1m.text} 1"
     }
 }
